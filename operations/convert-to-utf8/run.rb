21b7329b243ca91508dc35645408772e5762c7ca
Alberto Miedes <albertomg994@gmail.com>
2019-11-29 10:32:32 +0100

    convert-to-utf8 > Argument for fallback encoding

--- operations/convert-to-utf8/run.rb
+++ operations/convert-to-utf8/run.rb
@@ -12,35 +12,52 @@ Bundler.require
 #
 #  - 0: Input file
 #  - 1: Output file
+#  - 2: (Optional) Input file encoding. Used as fallback if autodetect fails.
 #
 # Samples:
 #
 #   /path/to/project/operations/convert-to-utf8/run.rb input_file output_file
 #
 
-def osx?
-  RbConfig::CONFIG["host_vendor"].downcase.include?("apple")
+class EncodingUtils
+  def self.detect_charset(file_path, fallback_charset = nil)
+    arg = osx? ? "I" : "i"
+
+    result = `file -#{arg} #{file_path}`
+
+    charset = result.strip.split("charset=").last
+
+    if unknown_charset?(charset) && fallback_charset.present?
+      fallback_charset
+    else
+      charset
+    end
+  rescue StandardError => e
+    raise "Can't determine charset of #{file_path}. Error: #{e.message}"
+  end
+
+  def self.utf8?(charset)
+    charset.downcase.gsub(/[^0-9a-z]/, "") == "utf8"
+  end
+
+  ## private
+
+  def self.osx?
+    RbConfig::CONFIG["host_vendor"].downcase.include?("apple")
+  end
+  private_class_method :osx?
+
+  def self.unknown_charset?(charset)
+    charset.downcase.include?("unknown")
+  end
+  private_class_method :unknown_charset?
 end
 
-def detect_charset(file_path)
-  arg = osx? ? "I" : "i"
-
-  result = `file -#{arg} #{file_path}`
-  result.strip.split("charset=").last
-rescue StandardError => e
-  raise "Can't determine charset of #{file_path}. Error: #{e.message}"
-end
-
-def utf8?(charset)
-  charset.downcase.gsub(/[^0-9a-z]/, "") == "utf8"
-end
-
-if ARGV.length != 2
-  raise "Review the arguments"
-end
+raise "Invalid number of arguments" unless ARGV.length.between?(2, 3)
 
 input_file = ARGV[0]
 output_file = ARGV[1]
+input_fallback_charset = ARGV[2]
 
 puts "[START] convert-to-utf8/run.rb input_file=#{input_file} output_file=#{output_file}"
 
@@ -48,9 +65,9 @@ if File.dirname(output_file) != "."
   FileUtils.mkdir_p(File.dirname(output_file))
 end
 
-charset = detect_charset(input_file)
+charset = EncodingUtils.detect_charset(input_file, input_fallback_charset)
 
-if utf8?(charset)
+if EncodingUtils.utf8?(charset)
   puts "Already in utf-8: #{input_file}"
   FileUtils.cp(input_file, output_file)
 else
