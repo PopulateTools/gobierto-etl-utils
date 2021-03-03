8b9505a76e2b6a73dc5ed1c3f34a8f48ac6a04e6
Víctor <victor@populate.tools>
2021-03-03 08:52:13 +0100

    We start sending just the category key and let the client do the translation to Spanish/Catalan/whatever it's needed.

--- operations/gobierto_data/extract-tenders/query.sql
+++ operations/gobierto_data/extract-tenders/query.sql
@@ -22,30 +22,7 @@ SELECT
   array_to_string(tenders.cpvs, ',') AS cpvs,
   process_types.text AS process_type,
   categories.id as category_id,
-  CASE categories.title
-    WHEN 'agriculture' THEN 'Servicios agrícolas y forestales'
-    WHEN 'architecture' THEN 'Arquitectura e ingeniería'
-    WHEN 'audiovisual' THEN 'Audiovisual'
-    WHEN 'catering' THEN 'Hostelería y restauración'
-    WHEN 'construction' THEN 'Construcción y mantenimiento'
-    WHEN 'culture' THEN 'Cultura y deporte'
-    WHEN 'education' THEN 'Enseñanza y formación'
-    WHEN 'electrical' THEN 'Equipos eléctricos y de iluminación'
-    WHEN 'environment' THEN 'Medio ambiente'
-    WHEN 'finance' THEN 'Servicios financieros y seguros'
-    WHEN 'furniture' THEN 'Mobiliario'
-    WHEN 'health' THEN 'Salud'
-    WHEN 'industry' THEN 'Industria y maquinaria'
-    WHEN 'legal' THEN 'Jurídico, contabilidad, mercadotecnia...'
-    WHEN 'other' THEN 'Otros'
-    WHEN 'print' THEN 'Impresión y material de oficina'
-    WHEN 'security' THEN 'Seguridad'
-    WHEN 'social_services' THEN 'Servicios sociales'
-    WHEN 'software' THEN 'Consultoría y desarrollo de software, equipos y licencias'
-    WHEN 'telecom' THEN 'Telecomunicaciones y correos'
-    WHEN 'textile' THEN 'Textil'
-    WHEN 'transportation' THEN 'Transporte'
-  END as category_title
+  categories.title as category_title
   FROM tenders
   LEFT JOIN fiscal_entities contractors ON contractor_id = contractors.id
   LEFT JOIN entity_types contractors_types ON contractors_types.id = contractors.entity_type
