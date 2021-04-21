312e560a275f6723755138a8dbb437266f6bae9a
Víctor <victor@populate.tools>
2021-04-21 09:25:14 +0200

    Since we changed the keys, the value of the text will change too.

--- operations/gobierto_data/extract-contracts/query.sql
+++ operations/gobierto_data/extract-contracts/query.sql
@@ -22,7 +22,7 @@ SELECT
   contract_types.text AS contract_type,
   process_types.text AS process_type,
   CASE process_types.text
-    WHEN 'Contrato menor' THEN true
+    WHEN 'minor_contract' THEN true
     ELSE false
   END as minor_contract,
   array_to_string(contracts.cpvs, ',') AS cpvs,
