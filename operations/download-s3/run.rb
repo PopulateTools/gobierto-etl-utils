09cc6ee0625dd7fb678e04d64aae67f35d720f24
Fernando Blat <fernando@blat.es>
2022-10-21 15:30:45 +0200

    Add a parameter to indicate the destination file name

--- operations/download-s3/run.rb
+++ operations/download-s3/run.rb
@@ -15,21 +15,23 @@ require "net/https"
 #
 #  - 0: S3 URI
 #  - 1: Output folder
+#  - 2: Output filename (optional)
 #
 # Samples:
 #
-#   /path/to/project/operations/download-s3/run.rb "getafe/providers" /tmp/foo
+#   /path/to/project/operations/download-s3/run.rb "getafe/providers" /tmp/foo [foo.txt]
 #
 
-if ARGV.length != 2
+if ARGV.length < 2 || ARGV.length > 3
   raise "Review the arguments"
 end
 
 folder = ARGV[0]
 destination_folder = ARGV[1]
+destination_file = ARGV[2]
 FileUtils.mkdir_p(destination_folder)
 
-puts "[START] download-s3/run.rb from #{folder} to #{destination_folder}"
+puts "[START] download-s3/run.rb from #{folder} to #{destination_folder}#{destination_file ? "/#{destination_file}" : ""}"
 
 s3 = Aws::S3::Resource.new(
   region: ENV.fetch("GOBIERTO_DATA_AWS_REGION"),
@@ -39,7 +41,7 @@ s3 = Aws::S3::Resource.new(
 
 objects = s3.bucket(ENV.fetch("GOBIERTO_DATA_S3_BUCKET_NAME")).objects(prefix: folder).each do |object|
   next if object.get.content_type == "application/x-directory"
-  destination_file = "#{destination_folder}/#{File.basename(object.public_url)}"
+  destination_file = "#{destination_folder}/#{destination_file || File.basename(object.public_url)}"
   puts "- Downloading #{object.public_url} in #{destination_file}"
   object.get(response_target: destination_file)
 end
