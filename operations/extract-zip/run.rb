14249758e774e60f163e4456b0f329876f3fc401
jbb231 <julieta@populate.tools>
2022-12-09 10:09:41 +0100

    remove size control

--- operations/extract-zip/run.rb
+++ operations/extract-zip/run.rb
@@ -27,8 +27,6 @@ Zip::File.open(zipfile_name) do |zip_file|
     # Handle entries one by one
     zip_file.each do |entry|
       puts "Extracting #{entry.name}"
-      raise 'File too large when extracted' if entry.size > MAX_SIZE
-  
       # Extract to file or directory based on name in the archive
       zip_file.extract(entry, destination_path)
     end
