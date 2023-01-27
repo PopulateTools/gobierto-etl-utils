948d47927ed4cfe77bbb39c52c0138a7ee2ccbdd
Fernando Blat <fernando@blat.es>
2023-01-27 04:28:20 +0100

    Accept a last argument with the raw table name

--- operations/load-csv-sqlite/run.rb
+++ operations/load-csv-sqlite/run.rb
@@ -13,33 +13,33 @@ require "fileutils"
 #  - 0: Data file (CSV)
 #  - 1: SQlite file path
 #  - 2: Record separator
+#  - 3: (Optional) Table name (default: data_raw)
 #
 # Samples:
 #
 #   /path/to/project/operations/load-csv-sqlite/run.rb /tmp/data_raw.csv /tmp/database.sqlite ','
 #
 
-if ARGV.length != 3
+if ARGV.length < 3 || ARGV.length > 4
   raise "Review the arguments"
 end
 
 data_path = ARGV[0]
 database_path = ARGV[1]
 separator = ARGV[2]
+raw_table_name = ARGV[3] || "data_raw"
 
-puts "[START] load-csv-sqlite/run.rb load CSV #{data_path} into #{database_path} with separator #{separator}"
+puts "[START] load-csv-sqlite/run.rb load CSV #{data_path} into #{database_path} with separator #{separator} in table #{raw_table_name}"
 
 unless File.file?(data_path)
   puts "[ERROR] #{data_path} doesn't exist"
 end
 
-FileUtils.rm_f(database_path)
-
 system(
 <<-COMMAND
 sqlite3 #{database_path} << EOF
 .separator "#{separator}"
-.import #{data_path} data_raw
+.import #{data_path} #{raw_table_name}
 EOF
 COMMAND
       )
