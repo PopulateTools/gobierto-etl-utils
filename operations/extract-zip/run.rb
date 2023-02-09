dcdd0824ad2b779f0ba686817a77bd4ec7a1c0d6
jbb231 <julieta@populate.tools>
2023-02-09 10:02:19 +0100

    bug extract action

--- operations/extract-zip/run.rb
+++ operations/extract-zip/run.rb
@@ -27,12 +27,12 @@ Zip.on_exists_proc = true
 Zip.continue_on_exists_proc = true
 
 Zip::File.open(zipfile_name) do |zip_file|
-    # Handle entries one by one
-    zip_file.each do |entry|
-      puts "Extracting #{entry.name}"
-      # Extract to file or directory based on name in the archive
-      zip_file.extract(entry, destination_path)
-    end
+  # Handle entries one by one
+  zip_file.each do |entry|
+    puts "Extracting #{entry.name}"
+    # Extract to file or directory based on name in the archive
+    entry.extract(destination_path)
   end
+end
 
 puts "[END] extract-zip/run.rb"
