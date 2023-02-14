17392d7728f6acffdd6565f10cd38cf845acfc9c
jbb231 <julieta@populate.tools>
2023-02-14 13:19:53 +0100

    click download operation
    This code finds a link to download an Excel file, click the link to initiate the download, and save the downloaded file.

---
+++ operations/click-download/run.rb
@@ -0,0 +1,38 @@
+require 'mechanize'
+
+# Usage:
+#
+#  - Must be ran as an independent Ruby script
+#  - Goes to url, clicks on text description button and save the downloaded excel
+#
+# Arguments:
+#
+#  - 0: URL
+#  - 1: Button's text description
+#  - 2: Name of the output file
+#
+# Samples:
+#
+#   /path/to/project/operations/click-download/run.rb https://example_url.es "Click here for download" /tmp/dataset.xlsx 
+#
+
+if ARGV.length != 3
+    raise "Review the arguments. Usage: ruby operations/click-download/run.rb https://example_url.es 'Click here for download' /tmp/dataset.xlsx "
+  end
+
+url, text_button, output_file = ARGV
+
+# Create a new Mechanize object
+agent = Mechanize.new
+
+# Load the page
+page = agent.get(url)
+
+# Find the link to download the Excel file
+download_link = page.link_with(text: text_button)
+
+# Click the link to download the Excel file
+excel_file = download_link.click
+
+# Save the Excel file
+File.open(output_file, 'wb') { |f| f.write(excel_file.body) }
